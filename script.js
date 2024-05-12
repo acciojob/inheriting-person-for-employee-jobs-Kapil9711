84c2b87404ccd49a1b75455aea0e4ebe06c8ea17
majestice_coder <142192726+Kapil9711@users.noreply.github.com>
2024-05-12 15:29:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ class Employee extends Person{
 	constructor(name,age,jobTitle)
 	{
 		super(name,age);
-		this.jobTitle = jobTitile;
+		this.jobTitle = jobTitle;
 	}
 	jobGreet(){console.log(`Hello, my name is  ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
 			  }
