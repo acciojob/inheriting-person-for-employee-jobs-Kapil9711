fb0c3e9a26a4d33057f5e4b63508fa13cbddb304
majestice_coder <142192726+Kapil9711@users.noreply.github.com>
2024-05-12 15:16:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // complete this js code
-function Person(name, age) {}
+class Person {
+	constructor(name,age){
+		this.name = name;
+		this.age = age;
+	}
+	greet(){
+		console.log(`Hello, my name ${this.name}, I am ${this.age} years old.`);
+	}
+		
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee {	
+	constructor(name,age,jobTitle)
+	{
+		super(name,age);
+		this.jobTitle = jobTitile;
+	}
+	jobGreet(){console.log(`Hello, my name is  ${this.name}, I am ${this.age} years old.`)
+			  }
+}
 
 // Do not change code below this line
 window.Person = Person;
